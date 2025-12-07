454a3accaae74beead8fad9c6b8d609d89961845
repen <9keepa@gmail.com>
2025-12-08 00:06:00 +0500

    develop

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/Iceloof/GoogleNews",
     packages=setuptools.find_packages(),
-    install_requires=['beautifulsoup4','dateparser','python-dateutil'],
+    install_requires=['beautifulsoup4','dateparser','python-dateutil', 'requests'],
     classifiers=[
         "Programming Language :: Python :: 3.6",
         "License :: OSI Approved :: MIT License",
